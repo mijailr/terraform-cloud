0038bd7cec43d0dfe2c5605dabab79e1e2e2cb50
Mijail Rondon <mijail@hey.com>
2020-08-14 00:28:38 -0500

    refactor(client): refactor Api Client

---
+++ src/api/TerraformCloudApi.ts
@@ -0,0 +1,38 @@
+import { AxiosInstance } from "axios";
+import { EventEmitter } from "events";
+import terraformCloudApiClient from "./terraformCloudApiClient";
+import { Account, Plan, Run } from "./types";
+
+export default class TerraformCloudApi extends EventEmitter {
+  private readonly client: AxiosInstance;
+
+  constructor(apiKey: string) {
+    super();
+    this.client = terraformCloudApiClient(apiKey);
+  }
+
+  async getAccount(): Promise<Account> {
+    const path = "/account/details";
+    return await this.get<Account>(path);
+  }
+
+  async getPlan(planId: string): Promise<Plan> {
+    const path = `/plans/${planId}`;
+    return await this.get<Plan>(path);
+  }
+
+  async getRun(runId: string): Promise<Run> {
+    const path = `/runs/${runId}`;
+    return await this.get<Run>(path);
+  }
+
+  async getRuns(workspaceId: string): Promise<Run[]> {
+    const path = `/workspaces/${workspaceId}/runs`;
+    return await this.get<Run[]>(path);
+  }
+
+  private async get<Res>(path: string): Promise<Res> {
+    const response = await this.client.get<Res>(path);
+    return response.data;
+  }
+}
---
+++ src/api/terraformCloudApiClient.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+
+const terraformCloudApiClient = (apiKey: string): AxiosInstance => {
+  const apiUrl = "https://app.terraform.io/api/v2";
+  const client: AxiosInstance = axios.create({ baseURL: apiUrl });
+  client.interceptors.request.use((req: AxiosRequestConfig) => {
+    req.headers = {
+      Authorization: `Bearer ${apiKey}`,
+      Accept: "application/json",
+    };
+    return req;
+  });
+
+  client.interceptors.response.use((res: AxiosResponse) => res.data);
+
+  return client;
+};
+
+export default terraformCloudApiClient;
