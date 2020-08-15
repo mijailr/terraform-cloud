937e476a69f46d261e2b22e6396c47ad2e519f93
Mijail Rondon <mijail@hey.com>
2020-08-15 15:06:06 -0500

    refactor(client): abstract class for requests

---
+++ src/api/Request.ts
@@ -0,0 +1,14 @@
+import { AxiosInstance } from 'axios'
+
+export abstract class Request {
+  constructor(private client: AxiosInstance) {}
+  protected async get<T>(path: string): Promise<T> {
+    const response = await this.client.get<T>(path)
+    return response.data
+  }
+
+  protected async patch<Entity, Request>(path: string, request: Request): Promise<Entity> {
+    const response = await this.client.patch(path, request)
+    return response.data
+  }
+}
