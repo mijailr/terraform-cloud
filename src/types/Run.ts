e4ae0e45e69e8eeeb20e2c1a54801099a2651fd4
Mijail Rondon <mijail@hey.com>
2020-08-15 20:41:46 -0500

    feat(Runs): add types definitions

--- src/types/Run.ts
+++ src/types/Run.ts
@@ -1,7 +1,35 @@
-import { TerraformCloudData, Relationship } from './TerraformCloudData'
+import { TerraformCloudData, Relationship, Type } from './TerraformCloudData'
 
 export type Run = TerraformCloudData<RunAttributes> & {
   relationships: RunRelationship
+}
+
+export type RunApplyRequest = {
+  comment: string | null
+}
+
+export type RunRequest = {
+  data: {
+    attributes: {
+      'is-destroy'?: boolean
+      message?: string
+      'target-addrs'?: string[]
+    }
+    relationships: {
+      workspace: {
+        data: {
+          id: string
+          type: Type.Workspaces
+        }
+      }
+      'configuration-version': {
+        data: {
+          id: string
+          type: Type.ConfigurationVersions
+        }
+      }
+    }
+  }
 }
 
 interface RunRelationship {
@@ -18,6 +46,25 @@ interface RunAttributes {
   isDestroy: boolean
   message: string
   source: string
-  status: string
+  status:
+    | 'pending'
+    | 'plan_queued'
+    | 'planning'
+    | 'planned'
+    | 'cost_estimating'
+    | 'cost_estimated'
+    | 'policy_checking'
+    | 'policy_override'
+    | 'policy_soft_failed'
+    | 'policy_checked'
+    | 'confirmed'
+    | 'planned_and_finished'
+    | 'apply_queued'
+    | 'applying'
+    | 'applied'
+    | 'discarded'
+    | 'errored'
+    | 'canceled'
+    | 'force_canceled'
   terraformVersion: string
 }
