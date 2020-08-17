eb39692f052e4232fdbf5637e82fe28bc0fb5b2f
Mijail Rondon <mijail@hey.com>
2020-08-17 00:42:19 -0500

    feat(workspaces): add create workspace endpoint

--- src/api/TerraformCloud.ts
+++ src/api/TerraformCloud.ts
@@ -3,11 +3,13 @@ import terraformCloudApiClient from './terraformCloudApiClient'
 import Account from './endpoints/Account'
 import Plans from './endpoints/Plans'
 import Runs from './endpoints/Runs'
+import Workspaces from './endpoints/Workspaces'
 
 export class TerraformCloud extends EventEmitter {
   public Account: Account
   public Plans: Plans
   public Runs: Runs
+  public Workspaces: Workspaces
 
   constructor(apiKey: string) {
     super()
@@ -15,5 +17,6 @@ export class TerraformCloud extends EventEmitter {
     this.Account = new Account(client)
     this.Plans = new Plans(client)
     this.Runs = new Runs(client)
+    this.Workspaces = new Workspaces(client)
   }
 }
