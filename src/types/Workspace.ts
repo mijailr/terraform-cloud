8bcfc4e72a3bfc0a3d73e33ec1f2bd90f28a98e2
Mijail Rondon <mijail@hey.com>
2020-08-17 00:40:53 -0500

    feat(workspaces): create workspaces types

---
+++ src/types/Workspace.ts
@@ -0,0 +1,58 @@
+import { Params, TerraformCloudData } from './TerraformCloudData'
+
+export type WorkspaceCreate = Params<'workspaces', WorkspaceCreateAttributes>
+
+export interface WorkspaceCreateAttributes {
+  name: string
+  agentPoolId?: string
+  allowDestroyPlan?: boolean
+  autoApply?: boolean
+  description?: string
+  executionMode?: string
+  fileTriggersEnabled?: boolean
+  sourceName?: string
+  sourceUrl?: string
+  queueAllRuns?: boolean
+  speculativeEnabled?: boolean
+  terraformVersion?: string
+  triggerPrefixes?: []
+  workingDirectory?: string
+  vcsRepo?: WorkspaceVcsRepoCreate
+}
+
+export interface WorkspaceVcsRepoCreate {
+  oauthTokenId: string
+  branch?: string
+  ingressSubmodules?: boolean
+  identifier: string
+}
+
+export type Workspace = TerraformCloudData<WorkspaceAttributes>
+
+export interface WorkspaceAttributes {
+  autoApply: boolean
+  allowDestroyPlan: boolean
+  canQueueDestroyPlan: boolean
+  createdAt: Date
+  environment: string
+  fileTriggersEnabled: boolean
+  locked: boolean
+  name: string
+  permissions: WorkspacePermissions
+  queueAllRuns: boolean
+  terraformVersion: string
+  triggerPrefixes: []
+  vcsRepo: null
+  workingDirectory: string
+  executionMode: string
+}
+
+export interface WorkspacePermissions {
+  canUpdate: boolean
+  canDestroy: boolean
+  canQueueDestroy: boolean
+  canQueueRun: boolean
+  canUpdateVariable: boolean
+  canLock: boolean
+  canReadSettings: boolean
+}
